14448e5391753221b891eedf171214c5737026ff
Chetan <ishunara25@gmail.com>
2026-06-01 14:52:34 +0530

    Add recursive Fibonacci series generator
    Implement a recursive function to generate Fibonacci series.

---
+++ 03_recursion/08_fibonacci.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+void func(int n, int temp, int arr[])
+{
+	if(temp>n)
+	return;
+	
+	arr[temp]=arr[temp-1]+arr[temp-2];
+	func(n, temp+1, arr);
+}
+
+int main()
+{
+	int n;
+	cout<<"Enter the number of times the fibonacci series to be printed: ";
+	cin>>n;
+	if(n==0)
+	{
+		cout<<"For input zero the series only contains :"<<n;
+		return 0;
+	}
+	int arr[n+1];
+	arr[0]=0;
+	arr[1]=1;
+	func(n, 2, arr);
+	cout<<"The fibonacci series is: ";
+	for(int i=0;i<=n;i++)
+	{
+		cout<<arr[i]<<" ";
+	}
+}
