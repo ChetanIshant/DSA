2af87877865ab42592d7869cc279cad1925a8cfd
Chetan <ishunara25@gmail.com>
2026-06-09 17:53:39 +0530

    Create 11_element_with_single_appearance.cpp

---
+++ 04_array/11_element_with_single_appearance.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+	int n, result=0;
+	cout << "Enter the size of array: ";
+	cin >> n;
+	vector<int> arr(n);
+	for(int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+	for(int i = 0;i < n; i++)
+	{
+		result = result ^ arr[i];
+	}
+	cout << "The element with single occurancy is : " << result;
+}
