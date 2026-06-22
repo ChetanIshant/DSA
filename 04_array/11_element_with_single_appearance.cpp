5e68ad05103fbf94913966de4b61ba5b0efbc3ab
Chetan <ishunara25@gmail.com>
2026-06-22 16:08:48 +0530

    Update 11_element_with_single_appearance.cpp

--- 04_array/11_element_with_single_appearance.cpp
+++ 04_array/11_element_with_single_appearance.cpp
@@ -3,24 +3,15 @@
 using namespace std;
 int main()
 {
-	int n, result=0;
+	int n, x, res = 0;
 	cout << "Enter the size of array: ";
 	cin >> n;
-	
-	vector<int> arr(n);
-	
+
 	for(int i = 0; i < n; i++)
 	{
-		cin >> arr[i];
-	}
-	for(int i = 0;i < n; i++)
-	{
-		result = result ^ arr[i];
-	}
-	cout << "The element with single occurancy is : " << result;
+		cin >> x;
+		res = res ^ x;	
+	}	
+	
+	cout << "The single appearance number is: " << res;
 }
-
-
-
-//this is not the final code
-//new code will be updated soon
