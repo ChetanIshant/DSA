c5bce8e64f284764ae0a848f138bff15669beafd
Chetan <ishunara25@gmail.com>
2026-06-23 15:07:05 +0530

    Create 14_two_sum.cpp

---
+++ 04_array/14_two_sum.cpp
@@ -0,0 +1,70 @@
+//We have multiple approaches for solving a two sum problem:
+//Very first which I have tried is two pointer approach, for sorted array it will work in O(n) but in-case of unsorted array, 
+//it will goes to O(n^2) which is not optimal
+//if we have to solve by using two pointer approach and in optimal complexoty, then the array must be sorted or we have to sort it first.
+
+
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+	int n, target, left = 0, right;
+	cout << "Enter the size of array: ";
+	cin >> n;
+	right = n - 1;
+	cout << "Enter the target value: ";
+	cin >> target;
+	vector<int> arr(n);
+	for(int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+	while(left < right)
+	{
+		if(arr[left] + arr[right] == target)
+		{
+			cout << "YES" << endl << "The indexes are: [" << left << ", " << right << "]";
+			return 0;
+		}
+		else if(arr[left] + arr[right] < target)
+		left += 1;
+		else
+		right -= 1;		
+	}
+	cout << "NO";
+}
+
+
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+	int n, target, ptr1 = 0, ptr2 = 0;
+	cout << "Enter the size of array: ";
+	cin >> n;
+	cout << "Enter the target value: ";
+	cin >> target;
+	vector<int> arr(n);
+	for(int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+	while(ptr1 < n - 1)
+	{
+		ptr2 = ptr1 + 1;
+		while(ptr2 < n)
+		{
+			if((arr[ptr1] + arr[ptr2]) == target)
+			{
+				cout << "YES" << endl << "The indexes are: [" << ptr1 << ", " << ptr2 << "]";
+				return 0;
+			}
+			ptr2 += 1;
+		}
+		ptr1 += 1;
+	}
+	cout << "NO";
+}
+*/
