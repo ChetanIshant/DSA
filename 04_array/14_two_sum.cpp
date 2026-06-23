c40d213b41d90a66b0167805e14a40d84727d635
Chetan <ishunara25@gmail.com>
2026-06-23 15:10:38 +0530

    Update 14_two_sum.cpp

--- 04_array/14_two_sum.cpp
+++ 04_array/14_two_sum.cpp
@@ -3,7 +3,7 @@
 //it will goes to O(n^2) which is not optimal
 //if we have to solve by using two pointer approach and in optimal complexoty, then the array must be sorted or we have to sort it first.
 
-//Two-pointer approach for sorted array
+//Two-pointer approach for sorted array, Complexity -> O(n)
 #include<iostream>
 #include<vector>
 using namespace std;
@@ -36,7 +36,7 @@ int main()
 }
 
 
-//Two-pointer approach for unsorted array Complexity -> O(n^2)
+//Two-pointer approach for unsorted array, Complexity -> O(n^2)
 #include<iostream>
 #include<vector>
 using namespace std;
