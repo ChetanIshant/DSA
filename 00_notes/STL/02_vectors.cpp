2ed3e0a7056401dcdba014c76e0b20683f25713f
Chetan <ishunara25@gmail.com>
2026-06-26 17:30:32 +0530

    Update 02_vectors.cpp

--- 00_notes/STL/02_vectors.cpp
+++ 00_notes/STL/02_vectors.cpp
@@ -6,3 +6,30 @@ For already known entries, array's are used but in case of unknown entries, or e
 It is a dynamic array that can automatically grow or shrink in size as elements are added or removed.
 Elements are stored in contigious memory as same as array
 */
+
+vector<int> vv;
+//This is a empty/dynamic container {}.
+
+v.push_back(1);
+v.emplace_back(2);
+// 1 is pushed in the vector v by push_back(1)  -> {1}
+// 2 is pushed in the vector v by emplace_back(2)  -> {1, 2} 
+// it is said that both the function does the same work, but emplace_back() is faster than push_back();
+
+vector<pair<int, int>> vec;
+// pair can also be defined as the data type of declaring the vector.
+vec.push_back({1, 2});
+vec.emplace_back(1, 2);
+//both the functions will do the same job but there is a small syntax difference.
+// and emplace_back will perform the operation of insertion more fastly as compared to push_back.
+
+vector<int> v(5,10);
+vector<data_type> name(size, value);
+//5 is the number of elements and 10 is the value of all the elements.
+
+//When the size of a vector is predefined and after that we will try to push_back(1), then the size will be increased and the value
+//will be inserted in the vector, as shown below:
+vector<int> v(5,10);
+// O/P -> [10, 10, 10, 10, 10]
+v.push_back(99);
+// O/P -> [10, 10, 10, 10, 10, 99]
