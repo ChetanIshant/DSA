acc961e63aac36b5fbb6f09bea6eb712ca977c0b
Chetan <ishunara25@gmail.com>
2026-06-27 17:13:19 +0530

    Update 02_vectors.cpp

--- 00_notes/STL/02_vectors.cpp
+++ 00_notes/STL/02_vectors.cpp
@@ -16,12 +16,6 @@ v.emplace_back(2);
 // 2 is pushed in the vector v by emplace_back(2)  -> {1, 2} 
 // it is said that both the function does the same work, but emplace_back() is faster than push_back();
 
-vector<pair<int, int>> vec;
-// pair can also be defined as the data type of declaring the vector.
-vec.push_back({1, 2});
-vec.emplace_back(1, 2);
-//both the functions will do the same job but there is a small syntax difference.
-// and emplace_back will perform the operation of insertion more fastly as compared to push_back.
 
 vector<int> v(5,10);
 vector<data_type> name(size, value);
