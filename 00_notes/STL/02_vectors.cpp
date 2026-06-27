a48e012c7ec3fa48ed8ff6dbca54cf79b5692561
Chetan <ishunara25@gmail.com>
2026-06-27 17:13:03 +0530

    Enhance comments in vector example
    Added comments explaining vector initialization, push_back behavior, and vector copying.

--- 00_notes/STL/02_vectors.cpp
+++ 00_notes/STL/02_vectors.cpp
@@ -27,4 +27,15 @@ vector<int> v(5,10);
 vector<data_type> name(size, value);
 //5 is the number of elements and 10 is the value of all the elements.
 
+//When the size of a vector is predefined and after that we will try to push_back(1), then the size will be increased and the value
+//will be inserted in the vector, as shown below:
+vector<int> v(5,10);
+// O/P -> [10, 10, 10, 10, 10]
+v.push_back(99);
+// O/P -> [10, 10, 10, 10, 10, 99]
+
+//We can also copy a existing vector to another new vector.
+vector<int> v1(5, 10);
+vector<int> v2(v1);
+//both the vectors have same data and v2 is the copy of v1.
 
