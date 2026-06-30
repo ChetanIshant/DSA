4cb0b356e368375400ac04ab63aed8e864df2f10
Chetan <ishunara25@gmail.com>
2026-06-30 16:02:42 +0530

    Update 02_vectors.cpp

--- 00_notes/STL/02_vectors.cpp
+++ 00_notes/STL/02_vectors.cpp
@@ -1,4 +1,11 @@
+/*
+Vector: dynamic array
+Vector is used when we don't know how many entries are to be done in the data structure.
+For already known entries, array's are used but in case of unknown entries, or entries will exceed array fails and vector comes up.
 
+It is a dynamic array that can automatically grow or shrink in size as elements are added or removed.
+Elements are stored in contigious memory as same as array
+*/
 
 vector<int> vv;
 //This is a empty/dynamic container {}.
