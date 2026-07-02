5f9daf7a28ad9991ab802de746e462a566ca0a45
Chetan <ishunara25@gmail.com>
2026-07-02 14:50:09 +0530

    Add examples for using pairs in C++
    Added examples of using pairs in C++ including single pairs, multiple pairs, and arrays of pairs.

--- 00_notes/STL/01_pairs.cpp
+++ 00_notes/STL/01_pairs.cpp
@@ -11,6 +11,24 @@ The first value is accessed using first, and the second value is accessed using 
 We use a pair when we want to keep two related pieces of information together as one object.
 */
 
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main()
+{
+	// for single pair
+	pair<int, float> p = {12, 10.1};
+	cout << "O/P single pair in one var: " << p.second << endl;
+	
+	// for multiple pair
+	pair<int, pair<int, int>> x = {1, {2, 3}};
+	cout << "O/P for multiple pair in one var: " << x.second.first << endl;
+	
+	// for array
+	pair<int, int> arr[] = {{2, 3}, {12, 13}, {22, 23}};
+	cout << "O/P for array of pairs: " << arr[1].second;
+}
 
 
 //for integer and string both in one single pair:
