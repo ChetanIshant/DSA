c246bcb8a6b5af3d0cef0c25aad76dfd5740e14a
Chetan <ishunara25@gmail.com>
2026-07-08 14:25:48 +0530

    Implement function to find single appearance number

--- 04_array/11_element_with_single_appearance.cpp
+++ 04_array/11_element_with_single_appearance.cpp
@@ -1,3 +1,17 @@
 #include<iostream>
 #include<vector>
 using namespace std;
+int main()
+{
+	int n, x, res = 0;
+	cout << "Enter the size of array: ";
+	cin >> n;
+
+	for(int i = 0; i < n; i++)
+	{
+		cin >> x;
+		res = res ^ x;	
+	}	
+	
+	cout << "The single appearance number is: " << res;
+}
