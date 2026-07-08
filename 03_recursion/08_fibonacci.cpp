0aab51b05a736a9862fdf7bea9324d60b9824f77
Chetan <ishunara25@gmail.com>
2026-07-08 14:26:41 +0530

    Implement Fibonacci series using recursion

--- 03_recursion/08_fibonacci.cpp
+++ 03_recursion/08_fibonacci.cpp
@@ -10,3 +10,23 @@ void func(int n, int temp, int arr[])
 	func(n, temp+1, arr);
 }
 
+int main()
+{
+	int n;
+	cout<<"Enter the number of times the fibonacci series to be printed: ";
+	cin>>n;
+	if(n==0)
+	{
+		cout<<"For input zero the series only contains :"<<n;
+		return 0;
+	}
+	int arr[n+1];
+	arr[0]=0;
+	arr[1]=1;
+	func(n, 2, arr);
+	cout<<"The fibonacci series is: ";
+	for(int i=0;i<=n;i++)
+	{
+		cout<<arr[i]<<" ";
+	}
+}
