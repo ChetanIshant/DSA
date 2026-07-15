081f29c1395655982dc9fdeaa0bd6da06fc45556
Chetan <ishunara25@gmail.com>
2026-07-15 17:22:25 +0530

    Update 14_two_sum.cpp

--- 04_array/14_two_sum.cpp
+++ 04_array/14_two_sum.cpp
@@ -91,7 +91,32 @@ int main()
     }
     cout << "Enter target: ";
     cin >> target;
+    // Sorting 
+    sort(arr.begin(), arr.end());
 
+	// Two Pointers
+    int left = 0;
+    int right = n - 1;
+    while(left < right)
+    {
+        int sum = arr[left] + arr[right];
+        if(sum == target)
+        {
+            cout << "YES\n";
+            cout << "Pair Found: "
+                 << arr[left] << " + "
+                 << arr[right] << " = "
+                 << target;
+            return 0;
+        }
+        else if(sum < target)
+        left++;
+        else
+		right--;
+    }
+    cout << "NO";
+    return 0;
+}	
 
 
 
