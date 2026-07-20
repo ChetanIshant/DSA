64fdae484ca70bdfe340f415df1d5b783033a6bf
Chetan <ishunara25@gmail.com>
2026-07-20 15:05:51 +0530

    Add recursive Fibonacci function
    Implement recursive function to calculate Fibonacci numbers.

--- 03_recursion/08_fibonacci.cpp
+++ 03_recursion/08_fibonacci.cpp
@@ -1,6 +1,14 @@
 #include<iostream>
 using namespace std;
 
+void func(int n, int temp, int arr[])
+{
+	if(temp>n)
+	return;
+	
+	arr[temp]=arr[temp-1]+arr[temp-2];
+	func(n, temp+1, arr);
+}
 
 int main()
 {
