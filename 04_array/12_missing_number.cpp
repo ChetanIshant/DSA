26c70906ecd92241112f344fdc3966900a9f1d70
Chetan <ishunara25@gmail.com>
2026-07-21 16:21:27 +0530

    Update 12_missing_number.cpp

--- 04_array/12_missing_number.cpp
+++ 04_array/12_missing_number.cpp
@@ -59,3 +59,27 @@ int main()
 using namespace std;
 
 int main()
+{
+    int n;
+    cout << "Enter the size of array: ";
+    cin >> n;
+
+    vector<int> arr(n - 1);
+
+    int xor1 = 0;
+    int xor2 = 0;
+
+    for(int i = 1; i <= n; i++)
+    {
+        xor1 ^= i; //xor1 = xor1 ^ i;
+    }
+
+	
+    for(int i = 0; i < n - 1; i++)
+    {
+        cin >> arr[i];
+        xor2 ^= arr[i]; //xor2 = xor2 ^ arr[i];
+    }
+
+    cout << "Missing Number = " << (xor1 ^ xor2);
+}
