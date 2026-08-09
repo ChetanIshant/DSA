f6b2512593fe1002a17b6c46dbbf5732e3485afa
Chetan <ishunara25@gmail.com>
2026-08-09 16:17:20 +0530

    Update 14_two_sum.cpp

--- 04_array/14_two_sum.cpp
+++ 04_array/14_two_sum.cpp
@@ -110,14 +110,7 @@ int main()
                  << target;
             return 0;
         }
-        else if(sum < target)
-        left++;
-        else
-		right--;
-    }
-    cout << "NO";
-    return 0;
-}	
+
 
 
 
